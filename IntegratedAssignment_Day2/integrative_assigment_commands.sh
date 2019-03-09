3e51f6a7e339506df5a0196e27acbd4dee1a2002
Heather <heathergibling@gmail.com>
2019-03-09 16:54:54 -0500

    update commands to match module 3

--- IntegratedAssignment_Day2/integrative_assigment_commands.sh
+++ IntegratedAssignment_Day2/integrative_assigment_commands.sh
@@ -1,197 +1,30 @@
-#set up
-export SOFT_DIR=/usr/local/
-export WORK_DIR=~/workspace/HTseq/Integrative_Assignment/
-export TRIMMOMATIC_JAR=$SOFT_DIR/Trimmomatic-0.36/trimmomatic-0.36.jar
-export PICARD_JAR=$SOFT_DIR/picard/picard.jar
-export GATK_JAR=$SOFT_DIR/gatk-4.0.1.2/gatk-package-4.0.1.2-local.jar
-export GATK_OLD_JAR=~/CourseData/HT_data/software/GenomeAnalysisTK-3.8/GenomeAnalysisTK.jar
-export BVATOOLS_JAR=~/CourseData/HT_data/software/bvatools-1.6/bvatools-1.6-full.jar
+### Set Up
+export WORK_DIR=~/workspace/HTseq/Module3/
 export REF=$WORK_DIR/reference/
 
 
 rm -rf $WORK_DIR
 mkdir -p $WORK_DIR
 cd $WORK_DIR
-ln -s ~/CourseData/HT_data/Module3/* .
+ln -s ~/CourseData/CG_data/HT_data/Module3/* .
 
-# Parent 1
-
-# Quality
-mkdir originalQC_NA12892/
-java -Xmx1G -jar ${BVATOOLS_JAR} readsqc \
-  --read1 raw_reads/NA12892/NA12892_CBW_chr1_R1.fastq.gz \
-  --read2 raw_reads/NA12892/NA12892_CBW_chr1_R2.fastq.gz \
-  --threads 2 --regionName ACTL8 --output originalQC_NA12892/
+singularity run -B ~/cvmfs_cache:/cvmfs-cache/ docker://c3genomics/genpipes:0.7  -V 3.1.2
+module load mugqic/java/openjdk-jdk1.8.0_72 mugqic/bvatools/1.6 mugqic/trimmomatic/0.36 mugqic/samtools/1.9 mugqic/bwa/0.7.17 mugqic/GenomeAnalysisTK/4.1.0.0 mugqic/R_Bioconductor/3.5.0_3.7
 
 
-#trim
+################
+### Parent 1 ###
+################
 
-cat $REF/adapters.fa
-
-mkdir -p reads/NA12892/
-
-java -Xmx2G -cp $TRIMMOMATIC_JAR org.usadellab.trimmomatic.TrimmomaticPE -threads 2 -phred33 \
-  raw_reads/NA12892/NA12892_CBW_chr1_R1.fastq.gz \
-  raw_reads/NA12892/NA12892_CBW_chr1_R2.fastq.gz \
-  reads/NA12892/NA12892_CBW_chr1_R1.t20l32.fastq.gz \
-  reads/NA12892/NA12892_CBW_chr1_S1.t20l32.fastq.gz \
-  reads/NA12892/NA12892_CBW_chr1_R2.t20l32.fastq.gz \
-  reads/NA12892/NA12892_CBW_chr1_S2.t20l32.fastq.gz \
-  ILLUMINACLIP:${REF}/adapters.fa:2:30:15 TRAILING:20 MINLEN:32 \
-  2> reads/NA12892/NA12892.trim.out
-
-cat reads/NA12892/NA12892.trim.out
-
-mkdir postTrimQC_NA12892/
-java -Xmx1G -jar ${BVATOOLS_JAR} readsqc \
-  --read1 reads/NA12892/NA12892_CBW_chr1_R1.t20l32.fastq.gz \
-  --read2 reads/NA12892/NA12892_CBW_chr1_R2.t20l32.fastq.gz \
-  --threads 2 --regionName ACTL8 --output postTrimQC_NA12892/
-
-
-# Alignment
-mkdir -p alignment/NA12892/
-
-bwa mem -M -t 2 \
-  -R '@RG\tID:NA12892\tSM:NA12892\tLB:NA12892\tPU:runNA12892_1\tCN:Broad Institute\tPL:ILLUMINA' \
-  ${REF}/hg19.fa \
-  reads/NA12892/NA12892_CBW_chr1_R1.t20l32.fastq.gz \
-  reads/NA12892/NA12892_CBW_chr1_R2.t20l32.fastq.gz \
-  | java -Xmx2G -jar ${GATK_JAR} SortSam \
-  -I /dev/stdin \
-  -O alignment/NA12892/NA12892.sorted.bam \
-  -SO coordinate \
-  --CREATE_INDEX true \
-  --MAX_RECORDS_IN_RAM=500000
-
-
-
-# Explore the bam files
-samtools view alignment/NA12892/NA12892.sorted.bam | head -n4
-
-# Count the *un-aligned* reads
-samtools view -c -f4 alignment/NA12892/NA12892.sorted.bam
-# Count the *aligned* reads 
-samtools view -c -F4 alignment/NA12892/NA12892.sorted.bam
-
-# Indel realignment
-# 1- Find the targets 2- Realign them.
-java -Xmx2G  -jar ${GATK_OLD_JAR} \
-  -T RealignerTargetCreator \
-  -R ${REF}/hg19.fa \
-  -o alignment/NA12892/realign.intervals \
-  -I alignment/NA12892/NA12892.sorted.bam \
-  -L chr1
-
-java -Xmx2G -jar ${GATK_OLD_JAR} \
-  -T IndelRealigner \
-  -R ${REF}/hg19.fa \
-  -targetIntervals alignment/NA12892/realign.intervals \
-  -o alignment/NA12892/NA12892.realigned.sorted.bam \
-  -I alignment/NA12892/NA12892.sorted.bam
-
-
-
-# FixMates (optional, see Module 3 main page for explanation)
-#java -Xmx2G -jar ${PICARD_JAR} FixMateInformation \
-#  VALIDATION_STRINGENCY=SILENT CREATE_INDEX=true SORT_ORDER=coordinate MAX_RECORDS_IN_RAM=500000 \
-#  INPUT=alignment/NA12892/NA12892.realigned.sorted.bam \
-#  OUTPUT=alignment/NA12892/NA12892.matefixed.sorted.bam
-
-# Mark duplicates
-java -Xmx2G -jar ${GATK_JAR} MarkDuplicates \
-  --REMOVE_DUPLICATES false --CREATE_INDEX true \
-  -I alignment/NA12892/NA12892.realigned.sorted.bam \
-  -O alignment/NA12892/NA12892.sorted.dup.bam \
-  --METRICS_FILE=alignment/NA12892/NA12892.sorted.dup.metrics
-
-# less alignment/NA12892/NA12892.sorted.dup.metrics
-
-
-# Recalibration
-java -Xmx2G -jar ${GATK_JAR} BaseRecalibrator \
-  -R ${REF}/hg19.fa \
-  --known-sites ${REF}/dbSNP_135_chr1.vcf.gz \
-  -L chr1:17704860-18004860 \
-  -O alignment/NA12892/NA12892.sorted.dup.recalibration_report.grp \
-  -I alignment/NA12892/NA12892.sorted.dup.bam
-
-java -Xmx2G -jar ${GATK_JAR} ApplyBQSR \
-  -R ${REF}/hg19.fa \
-  -bqsr alignment/NA12892/NA12892.sorted.dup.recalibration_report.grp \
-  -O alignment/NA12892/NA12892.sorted.dup.recal.bam \
-  -I alignment/NA12892/NA12892.sorted.dup.bam
-
-
-
-
-##emit 1 warning for the dictionnary
-#not working
-
-#java -Xmx2G -jar ${GATK_JAR} \
-#  -T PrintReads \
-#  -nct 2 \
-#  -R ${REF}/hg19.fa \
-#  -BQSR alignment/NA12892/NA12892.sorted.dup.recalibration_report.grp \
-#  -o alignment/NA12892/NA12892.sorted.dup.recal.bam \
-#  -I alignment/NA12892/NA12892.sorted.dup.bam
-
-
-# Extract Metrics
-java  -Xmx2G -jar ${GATK_OLD_JAR} \
-  -T DepthOfCoverage \
-  --omitDepthOutputAtEachBase \
-  --summaryCoverageThreshold 10 \
-  --summaryCoverageThreshold 25 \
-  --summaryCoverageThreshold 50 \
-  --summaryCoverageThreshold 100 \
-  --start 1 --stop 500 --nBins 499 -dt NONE \
-  -R ${REF}/hg19.fa \
-  -o alignment/NA12892/NA12892.sorted.dup.recal.coverage \
-  -I alignment/NA12892/NA12892.sorted.dup.recal.bam \
-  -L chr1:17700000-18100000
-  
-## less -S alignment/NA12892/NA12892.sorted.dup.recal.coverage.sample_interval_summary
-
-
-java -Xmx2G -jar ${GATK_JAR} CollectInsertSizeMetrics \
-  -R ${REF}/hg19.fa \
-  -I alignment/NA12892/NA12892.sorted.dup.recal.bam \
-  -O alignment/NA12892/NA12892.sorted.dup.recal.metric.insertSize.tsv \
-  -H alignment/NA12892/NA12892.sorted.dup.recal.metric.insertSize.histo.pdf \
-  --METRIC_ACCUMULATION_LEVEL LIBRARY
-
-
-
-  
-## less -S alignment/NA12892/NA12892.sorted.dup.recal.metric.insertSize.tsv
-# head -9  alignment/NA12892/NA12892.sorted.dup.recal.metric.insertSize.tsv | tail -3 | cut -f5,6
-
-java -Xmx2G -jar ${GATK_JAR} CollectAlignmentSummaryMetrics \
-  -R ${REF}/hg19.fa \
-  -I alignment/NA12892/NA12892.sorted.dup.recal.bam \
-  -O alignment/NA12892/NA12892.sorted.dup.recal.metric.alignment.tsv \
-  --METRIC_ACCUMULATION_LEVEL LIBRARY
-
-
-## less -S alignment/NA12892/NA12892.sorted.dup.recal.metric.alignment.tsv
-# head -10  alignment/NA12892/NA12892.sorted.dup.recal.metric.alignment.tsv | tail -4 | cut -f7
-
-
-
-  
-### parent 2
-
-
-# Quality
-mkdir originalQC_NA12891/
+### Quality
+mkdir -p originalQC/
 java -Xmx1G -jar ${BVATOOLS_JAR} readsqc \
   --read1 raw_reads/NA12891/NA12891_CBW_chr1_R1.fastq.gz \
   --read2 raw_reads/NA12891/NA12891_CBW_chr1_R2.fastq.gz \
-  --threads 2 --regionName ACTL8 --output originalQC_NA12891/
+  --threads 2 --regionName ACTL8 --output originalQC/
 
 
-#trim
+### Trim
 
 cat $REF/adapters.fa
 
@@ -209,14 +42,15 @@ java -Xmx2G -cp $TRIMMOMATIC_JAR org.usadellab.trimmomatic.TrimmomaticPE -thread
 
 cat reads/NA12891/NA12891.trim.out
 
-mkdir postTrimQC_NA12891/
+
+mkdir postTrimQC/
 java -Xmx1G -jar ${BVATOOLS_JAR} readsqc \
   --read1 reads/NA12891/NA12891_CBW_chr1_R1.t20l32.fastq.gz \
   --read2 reads/NA12891/NA12891_CBW_chr1_R2.t20l32.fastq.gz \
-  --threads 2 --regionName ACTL8 --output postTrimQC_NA12891/
+  --threads 2 --regionName ACTL8 --output postTrimQC/
 
 
-# Alignment
+### Alignment
 mkdir -p alignment/NA12891/
 
 bwa mem -M -t 2 \
@@ -228,44 +62,52 @@ bwa mem -M -t 2 \
   -I /dev/stdin \
   -O alignment/NA12891/NA12891.sorted.bam \
   -SO coordinate \
-  --CREATE_INDEX true \
-  --MAX_RECORDS_IN_RAM=500000
+  --CREATE_INDEX true --MAX_RECORDS_IN_RAM 500000
 
 
+# explore the bam files
+samtools view alignment/NA12891/NA12891.sorted.bam | head -n2
 
-# Explore the bam files
-samtools view alignment/NA12891/NA12891.sorted.bam | head -n4
-
-# Count the *un-aligned* reads
+# count the un-aligned reads
 samtools view -c -f4 alignment/NA12891/NA12891.sorted.bam
-# Count the *aligned* reads 
+
+# count the aligned reads
 samtools view -c -F4 alignment/NA12891/NA12891.sorted.bam
 
-# Indel realignment
-# 1- Find the targets 2- Realign them.
-java -Xmx2G  -jar ${GATK_OLD_JAR} \
+
+### Indel Realignment
+
+# switch to old GATK 3.8
+module unload  mugqic/GenomeAnalysisTK/4.1.0.0
+module load mugqic/GenomeAnalysisTK/3.8
+
+java -Xmx2G  -jar ${GATK_JAR} \
   -T RealignerTargetCreator \
   -R ${REF}/hg19.fa \
   -o alignment/NA12891/realign.intervals \
   -I alignment/NA12891/NA12891.sorted.bam \
   -L chr1
 
-java -Xmx2G -jar ${GATK_OLD_JAR} \
+java -Xmx2G -jar ${GATK_JAR} \
   -T IndelRealigner \
   -R ${REF}/hg19.fa \
   -targetIntervals alignment/NA12891/realign.intervals \
   -o alignment/NA12891/NA12891.realigned.sorted.bam \
   -I alignment/NA12891/NA12891.sorted.bam
 
+# return to GATK 4
+module unload mugqic/GenomeAnalysisTK/3.8
+module load  mugqic/GenomeAnalysisTK/4.1.0.0
 
 
-# FixMates (optional, see Module 3 main page for explanation)
-#java -Xmx2G -jar ${PICARD_JAR} FixMateInformation \
+### FixMates (optional)
+# java -Xmx2G -jar ${GATK_JAR} FixMateInformation \
 #  VALIDATION_STRINGENCY=SILENT CREATE_INDEX=true SORT_ORDER=coordinate MAX_RECORDS_IN_RAM=500000 \
 #  INPUT=alignment/NA12891/NA12891.realigned.sorted.bam \
 #  OUTPUT=alignment/NA12891/NA12891.matefixed.sorted.bam
 
-# Mark duplicates
+
+### Mark Duplicates
 java -Xmx2G -jar ${GATK_JAR} MarkDuplicates \
   --REMOVE_DUPLICATES false --CREATE_INDEX true \
   -I alignment/NA12891/NA12891.realigned.sorted.bam \
@@ -274,8 +116,7 @@ java -Xmx2G -jar ${GATK_JAR} MarkDuplicates \
 
 # less alignment/NA12891/NA12891.sorted.dup.metrics
 
-
-# Recalibration
+### Recalibration
 java -Xmx2G -jar ${GATK_JAR} BaseRecalibrator \
   -R ${REF}/hg19.fa \
   --known-sites ${REF}/dbSNP_135_chr1.vcf.gz \
@@ -290,22 +131,13 @@ java -Xmx2G -jar ${GATK_JAR} ApplyBQSR \
   -I alignment/NA12891/NA12891.sorted.dup.bam
 
 
+### Extract Metrics
 
+# switch to old GATK 3.8
+module unload mugqic/GenomeAnalysisTK/4.1.0.0
+module load mugqic/GenomeAnalysisTK/3.8
 
-##emit 1 warning for the dictionnary
-#not working
-
-#java -Xmx2G -jar ${GATK_JAR} \
-#  -T PrintReads \
-#  -nct 2 \
-#  -R ${REF}/hg19.fa \
-#  -BQSR alignment/NA12891/NA12891.sorted.dup.recalibration_report.grp \
-#  -o alignment/NA12891/NA12891.sorted.dup.recal.bam \
-#  -I alignment/NA12891/NA12891.sorted.dup.bam
-
-
-# Extract Metrics
-java  -Xmx2G -jar ${GATK_OLD_JAR} \
+java  -Xmx2G -jar ${GATK_JAR} \
   -T DepthOfCoverage \
   --omitDepthOutputAtEachBase \
   --summaryCoverageThreshold 10 \
@@ -317,9 +149,12 @@ java  -Xmx2G -jar ${GATK_OLD_JAR} \
   -o alignment/NA12891/NA12891.sorted.dup.recal.coverage \
   -I alignment/NA12891/NA12891.sorted.dup.recal.bam \
   -L chr1:17700000-18100000
-  
-## less -S alignment/NA12891/NA12891.sorted.dup.recal.coverage.sample_interval_summary
 
+# return to GATK 4
+module unload mugqic/GenomeAnalysisTK/3.8
+module load mugqic/GenomeAnalysisTK/4.1.0.0
+
+# less -S alignment/NA12891/NA12891.sorted.dup.recal.coverage.sample_interval_summary
 
 java -Xmx2G -jar ${GATK_JAR} CollectInsertSizeMetrics \
   -R ${REF}/hg19.fa \
@@ -327,12 +162,8 @@ java -Xmx2G -jar ${GATK_JAR} CollectInsertSizeMetrics \
   -O alignment/NA12891/NA12891.sorted.dup.recal.metric.insertSize.tsv \
   -H alignment/NA12891/NA12891.sorted.dup.recal.metric.insertSize.histo.pdf \
   --METRIC_ACCUMULATION_LEVEL LIBRARY
-
-
-
   
-## less -S alignment/NA12891/NA12891.sorted.dup.recal.metric.insertSize.tsv
-# head -9  alignment/NA12891/NA12891.sorted.dup.recal.metric.insertSize.tsv | tail -3 | cut -f5,6
+# less -S alignment/NA12891/NA12891.sorted.dup.recal.metric.insertSize.tsv
 
 java -Xmx2G -jar ${GATK_JAR} CollectAlignmentSummaryMetrics \
   -R ${REF}/hg19.fa \
@@ -340,11 +171,172 @@ java -Xmx2G -jar ${GATK_JAR} CollectAlignmentSummaryMetrics \
   -O alignment/NA12891/NA12891.sorted.dup.recal.metric.alignment.tsv \
   --METRIC_ACCUMULATION_LEVEL LIBRARY
 
-
-## less -S alignment/NA12891/NA12891.sorted.dup.recal.metric.alignment.tsv
-# head -10  alignment/NA12891/NA12891.sorted.dup.recal.metric.alignment.tsv | tail -4 | cut -f7
+# less -S alignment/NA12891/NA12891.sorted.dup.recal.metric.alignment.tsv
 
 
 
+################
+### Parent 2 ###
+################
+
+### Quality
+mkdir -p originalQC/
+java -Xmx1G -jar ${BVATOOLS_JAR} readsqc \
+  --read1 raw_reads/NA12892/NA12892_CBW_chr1_R1.fastq.gz \
+  --read2 raw_reads/NA12892/NA12892_CBW_chr1_R2.fastq.gz \
+  --threads 2 --regionName ACTL8 --output originalQC/
 
 
+### Trim
+
+cat $REF/adapters.fa
+
+mkdir -p reads/NA12892/
+
+java -Xmx2G -cp $TRIMMOMATIC_JAR org.usadellab.trimmomatic.TrimmomaticPE -threads 2 -phred33 \
+  raw_reads/NA12892/NA12892_CBW_chr1_R1.fastq.gz \
+  raw_reads/NA12892/NA12892_CBW_chr1_R2.fastq.gz \
+  reads/NA12892/NA12892_CBW_chr1_R1.t20l32.fastq.gz \
+  reads/NA12892/NA12892_CBW_chr1_S1.t20l32.fastq.gz \
+  reads/NA12892/NA12892_CBW_chr1_R2.t20l32.fastq.gz \
+  reads/NA12892/NA12892_CBW_chr1_S2.t20l32.fastq.gz \
+  ILLUMINACLIP:${REF}/adapters.fa:2:30:15 TRAILING:20 MINLEN:32 \
+  2> reads/NA12892/NA12892.trim.out
+
+cat reads/NA12892/NA12892.trim.out
+
+
+mkdir postTrimQC/
+java -Xmx1G -jar ${BVATOOLS_JAR} readsqc \
+  --read1 reads/NA12892/NA12892_CBW_chr1_R1.t20l32.fastq.gz \
+  --read2 reads/NA12892/NA12892_CBW_chr1_R2.t20l32.fastq.gz \
+  --threads 2 --regionName ACTL8 --output postTrimQC/
+
+
+### Alignment
+mkdir -p alignment/NA12892/
+
+bwa mem -M -t 2 \
+  -R '@RG\tID:NA12892\tSM:NA12892\tLB:NA12892\tPU:runNA12892_1\tCN:Broad Institute\tPL:ILLUMINA' \
+  ${REF}/hg19.fa \
+  reads/NA12892/NA12892_CBW_chr1_R1.t20l32.fastq.gz \
+  reads/NA12892/NA12892_CBW_chr1_R2.t20l32.fastq.gz \
+  | java -Xmx2G -jar ${GATK_JAR} SortSam \
+  -I /dev/stdin \
+  -O alignment/NA12892/NA12892.sorted.bam \
+  -SO coordinate \
+  --CREATE_INDEX true --MAX_RECORDS_IN_RAM 500000
+
+
+# explore the bam files
+samtools view alignment/NA12892/NA12892.sorted.bam | head -n2
+
+# count the un-aligned reads
+samtools view -c -f4 alignment/NA12892/NA12892.sorted.bam
+
+# count the aligned reads
+samtools view -c -F4 alignment/NA12892/NA12892.sorted.bam
+
+
+### Indel Realignment
+
+# switch to old GATK 3.8
+module unload  mugqic/GenomeAnalysisTK/4.1.0.0
+module load mugqic/GenomeAnalysisTK/3.8
+
+java -Xmx2G  -jar ${GATK_JAR} \
+  -T RealignerTargetCreator \
+  -R ${REF}/hg19.fa \
+  -o alignment/NA12892/realign.intervals \
+  -I alignment/NA12892/NA12892.sorted.bam \
+  -L chr1
+
+java -Xmx2G -jar ${GATK_JAR} \
+  -T IndelRealigner \
+  -R ${REF}/hg19.fa \
+  -targetIntervals alignment/NA12892/realign.intervals \
+  -o alignment/NA12892/NA12892.realigned.sorted.bam \
+  -I alignment/NA12892/NA12892.sorted.bam
+
+# return to GATK 4
+module unload mugqic/GenomeAnalysisTK/3.8
+module load  mugqic/GenomeAnalysisTK/4.1.0.0
+
+
+### FixMates (optional)
+# java -Xmx2G -jar ${GATK_JAR} FixMateInformation \
+#  VALIDATION_STRINGENCY=SILENT CREATE_INDEX=true SORT_ORDER=coordinate MAX_RECORDS_IN_RAM=500000 \
+#  INPUT=alignment/NA12892/NA12892.realigned.sorted.bam \
+#  OUTPUT=alignment/NA12892/NA12892.matefixed.sorted.bam
+
+
+### Mark Duplicates
+java -Xmx2G -jar ${GATK_JAR} MarkDuplicates \
+  --REMOVE_DUPLICATES false --CREATE_INDEX true \
+  -I alignment/NA12892/NA12892.realigned.sorted.bam \
+  -O alignment/NA12892/NA12892.sorted.dup.bam \
+  --METRICS_FILE=alignment/NA12892/NA12892.sorted.dup.metrics
+
+# less alignment/NA12892/NA12892.sorted.dup.metrics
+
+### Recalibration
+java -Xmx2G -jar ${GATK_JAR} BaseRecalibrator \
+  -R ${REF}/hg19.fa \
+  --known-sites ${REF}/dbSNP_135_chr1.vcf.gz \
+  -L chr1:17704860-18004860 \
+  -O alignment/NA12892/NA12892.sorted.dup.recalibration_report.grp \
+  -I alignment/NA12892/NA12892.sorted.dup.bam
+
+java -Xmx2G -jar ${GATK_JAR} ApplyBQSR \
+  -R ${REF}/hg19.fa \
+  -bqsr alignment/NA12892/NA12892.sorted.dup.recalibration_report.grp \
+  -O alignment/NA12892/NA12892.sorted.dup.recal.bam \
+  -I alignment/NA12892/NA12892.sorted.dup.bam
+
+
+### Extract Metrics
+
+# switch to old GATK 3.8
+module unload mugqic/GenomeAnalysisTK/4.1.0.0
+module load mugqic/GenomeAnalysisTK/3.8
+
+java  -Xmx2G -jar ${GATK_JAR} \
+  -T DepthOfCoverage \
+  --omitDepthOutputAtEachBase \
+  --summaryCoverageThreshold 10 \
+  --summaryCoverageThreshold 25 \
+  --summaryCoverageThreshold 50 \
+  --summaryCoverageThreshold 100 \
+  --start 1 --stop 500 --nBins 499 -dt NONE \
+  -R ${REF}/hg19.fa \
+  -o alignment/NA12892/NA12892.sorted.dup.recal.coverage \
+  -I alignment/NA12892/NA12892.sorted.dup.recal.bam \
+  -L chr1:17700000-18100000
+
+# return to GATK 4
+module unload mugqic/GenomeAnalysisTK/3.8
+module load mugqic/GenomeAnalysisTK/4.1.0.0
+
+# less -S alignment/NA12892/NA12892.sorted.dup.recal.coverage.sample_interval_summary
+
+java -Xmx2G -jar ${GATK_JAR} CollectInsertSizeMetrics \
+  -R ${REF}/hg19.fa \
+  -I alignment/NA12892/NA12892.sorted.dup.recal.bam \
+  -O alignment/NA12892/NA12892.sorted.dup.recal.metric.insertSize.tsv \
+  -H alignment/NA12892/NA12892.sorted.dup.recal.metric.insertSize.histo.pdf \
+  --METRIC_ACCUMULATION_LEVEL LIBRARY
+  
+# less -S alignment/NA12892/NA12892.sorted.dup.recal.metric.insertSize.tsv
+
+java -Xmx2G -jar ${GATK_JAR} CollectAlignmentSummaryMetrics \
+  -R ${REF}/hg19.fa \
+  -I alignment/NA12892/NA12892.sorted.dup.recal.bam \
+  -O alignment/NA12892/NA12892.sorted.dup.recal.metric.alignment.tsv \
+  --METRIC_ACCUMULATION_LEVEL LIBRARY
+
+# less -S alignment/NA12892/NA12892.sorted.dup.recal.metric.alignment.tsv
+
+
+### Quit the singularity container environment
+
+exit
